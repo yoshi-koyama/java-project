af08f73abed29939f139e2789b9e97bb112652ec
yoshi-koyama <koyama@reytech.co.jp>
2025-02-16 22:33:23 +0900

    0から10までの偶数のみを出力するように変更

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,8 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 } 
