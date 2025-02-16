fb9a07468981aca8525185d348f72ca3b8e0c7b5
yoshi-koyama <koyama@reytech.co.jp>
2025-02-16 22:30:05 +0900

    forループを使用して0から4までの数字を出力するように変更

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("こんにちは");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 } 
